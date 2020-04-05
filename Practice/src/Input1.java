690ec6aee3b99406be87ab4320b2239e034995fe
Tarren <cct557@gmail.com>
2020-04-05 18:09:35 +1000

    initial commit

---
+++ Practice/src/Input1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Input1 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		Scanner reader = new Scanner(System.in);
+
+        // Identifying the input values and declaring the variables for them
+        int first = 1;
+        int second = 2;
+        
+
+        // Assigning the user input to the variables
+        System.out.print("Enter firt:");
+        
+        first = Integer.valueOf(reader.nextLine());
+        
+        System.out.print("Enter second:");
+
+        second = Integer.valueOf(reader.nextLine());
+        // Identifying the operation and declaring a variable for the result
+        int product = first * second;
+
+        // Printing the result of the operation
+        System.out.println("The product of " + first + " and " + second + " is " + product);
+
+	}
+
+}
